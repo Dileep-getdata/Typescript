6468fa4c56c50dfd55364e36afa977c6d571b9a0
Dileep Thiriveedhi <dileep2424@gmail.com>
2022-11-23 11:47:06 +0530

    first typescript

---
+++ app.js
@@ -0,0 +1,21 @@
+"use strict";
+const no1 = document.getElementById('num1');
+const no2 = document.getElementById('num2');
+const button = document.querySelector('button');
+function add(num1, num2) {
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    return +num1 + +num2;
+}
+button.addEventListener('click', () => {
+    const num1 = no1.value;
+    const num2 = no2.value;
+    const result = add(+num1, +num2);
+    const strngrsult = add(num1, num2);
+    console.log(result);
+    console.log(strngrsult);
+});
