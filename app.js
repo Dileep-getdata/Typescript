b54a51ebedecb9453f13a59febb3dec99a24877d
Dileep Thiriveedhi <dileep2424@gmail.com>
2022-11-23 16:49:06 +0530

    type script  type keyword

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const no1 = document.getElementById('num1');
 const no2 = document.getElementById('num2');
 const button = document.querySelector('button');
+const Result = [];
+const strinresult = [];
 function add(num1, num2) {
     if (typeof num1 === 'number' && typeof num2 === 'number') {
         return num1 + num2;
@@ -11,11 +13,17 @@ function add(num1, num2) {
     }
     return +num1 + +num2;
 }
+function printDisplay(relObj) {
+    console.log(relObj.val);
+}
 button.addEventListener('click', () => {
     const num1 = no1.value;
     const num2 = no2.value;
     const result = add(+num1, +num2);
     const strngrsult = add(num1, num2);
+    Result.push(result);
+    strinresult.push(strngrsult);
+    printDisplay({ val: result, date: new Date() });
     console.log(result);
     console.log(strngrsult);
 });
--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
 const no1=document.getElementById('num1') as HTMLInputElement;
 const no2=document.getElementById('num2') as HTMLInputElement;
 const button=document.querySelector('button')!;
+const Result:number[]=[];
+const strinresult:string[]=[];
 
+type numOrString=number | string;
 
-function add(num1:number |string,num2:number|string){
+function add(num1:numOrString,num2:numOrString){
     if(typeof num1==='number' && typeof num2==='number'){
         return num1 + num2;
     }else if(typeof num1==='string' && typeof num2==='string'){
@@ -12,11 +15,18 @@ function add(num1:number |string,num2:number|string){
     return +num1 + +num2;
 }
 
+function printDisplay(relObj:{val:number,date:Date}){
+    console.log(relObj.val);
+}
+
 button.addEventListener('click',()=>{
     const num1=no1.value;
     const num2=no2.value;
     const result=add(+num1,+num2);
     const strngrsult=add(num1,num2);
+    Result.push(result as number);
+    strinresult.push(strngrsult as string);
+    printDisplay({val:result as number,date:new Date() as Date});
     console.log(result);
     console.log(strngrsult);
 })
